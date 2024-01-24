c1679dd9218141220dbda98b8b4674c1addf2fc8
sarthakrautela <120566373+sarthakrautela@users.noreply.github.com>
2024-01-24 21:55:55 +0530

    new

---
+++ projects/starbelly/Starbelly Contact/JavaScript/star.js
@@ -0,0 +1,19 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('.notifaction-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
